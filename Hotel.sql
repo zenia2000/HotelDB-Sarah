69568200e8130c616b18d570420563af8e5d8856
Sarah <sarahenya2000@hotmail.dk>
2023-03-02 17:06:27 +0100

    Add project files.

---
+++ Hotel.sql
@@ -0,0 +1,88 @@
+﻿use HotelDB;
+
+--1
+Select * From Hotel;
+
+--2
+Select * From Hotel
+Where Address like '%Roskilde%';
+
+--3
+Select * From Guest
+Where Address like '%Roskilde%';
+
+--4
+Select * From Guest
+Where Address like '%Roskilde%'
+ORDER BY Name;
+
+--5
+Select * From Room
+Where Types = 'D' and Price <= '200';
+
+--6
+Select * From Room
+Where Price<400 And (Types = 'D' OR Types ='F');
+
+--7
+Select * From Room
+Where Price<400 And (Types = 'D' OR Types ='F')
+Order By Price;
+
+--8
+Select * From Guest
+Where Name Like 'G%';
+
+--9
+Select Count(Hotel_No)
+From Hotel;
+
+--10
+Select Count(Hotel_No)
+From Hotel
+Where Address like '%Roskilde%';
+
+--11
+Select AVG(Price)
+From Room;
+
+--12
+Select AVG(Price)
+From Room
+Where Types = 'S';
+
+--13
+Select AVG(Price)
+From Room
+Where Types = 'D';
+
+--14
+Select AVG(Price)
+From Room
+Where Hotel_No = '7';
+
+--15
+Select SUM(Price)
+From room 
+Where types = 'D';
+
+--16
+Select * From Booking
+Where Date_From > '2011-03-01' And Date_To < '2011-03-31';
+
+--17
+Select * From Booking
+Where Date_From = '2011-02-26' And Hotel_No ='7';
+
+--18
+Select * From Booking
+Where Date_From = '2011-02-27' And Hotel_No ='7';
+
+--19
+Select * From Room
+Where Hotel_No = '6';
+
+--20
+Select COUNT(Guest_No)
+From Booking
+Where Hotel_No = '6';
---
+++ SQLQuery1.sql
@@ -0,0 +1,40 @@
+﻿
+use HotelDB;
+
+CREATE TABLE Hotel(
+     Hotel_No  int  NOT NULL PRIMARY KEY,
+     Name      VARCHAR(30)     NOT NULL,
+     Address   VARCHAR(50)  NOT NULL
+);
+
+CREATE TABLE Room(
+	 Room_No   int    NOT NULL,
+     Hotel_No  int    NOT NULL,
+     Types     CHAR(1)   DEFAULT 'S',
+     Price     FLOAT,
+	CONSTRAINT checkType 
+	CHECK (Types IN ('D','F','S') OR Types IS NULL),
+	CONSTRAINT checkPrice 
+	CHECK (price BETWEEN 0 AND 9999),
+
+	FOREIGN KEY (Hotel_No) REFERENCES Hotel (Hotel_No)
+	ON UPDATE CASCADE ON DELETE NO ACTION,
+	Primary KEY (Room_No, Hotel_No)
+);
+
+CREATE TABLE Guest (
+     Guest_No  int  NOT NULL PRIMARY KEY,
+     Name      VARCHAR(30)      NOT NULL,
+     Address   VARCHAR(50)   NOT NULL
+);
+
+CREATE TABLE Booking(
+     Booking_id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	 Hotel_No  int   NOT NULL,
+     Guest_No  int   NOT NULL,
+     Date_From DATE  NOT NULL,
+     Date_To   DATE  NOT NULL,
+     Room_No   int   NOT NULL,
+     FOREIGN KEY(Guest_No) REFERENCES Guest(Guest_No),
+	 FOREIGN KEY(Room_No, Hotel_No) REFERENCES Room(Room_No, Hotel_No)
+);
